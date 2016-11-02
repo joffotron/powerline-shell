15cf1c8c034dcc73b1ff0fd55e93576f3ffeaa88
Joseph Miller <joff@googlehax.com>
2016-11-02 11:24:15 +1100

    Fix indentation

--- segments/aws_profile.py
+++ segments/aws_profile.py
@@ -2,8 +2,8 @@ import os
 
 def add_aws_profile_segment(powerline):
   env = os.getenv('AWS_CONFIG_FILE')
-    if env is None:
-        return
+  if env is None:
+    return
 
   default_profile = os.getenv('AWS_DEFAULT_PROFILE') or 'default'
   powerline.append(' %s ' % default_profile )
