06173ea045eded1be1b201d606e746ba44dbfd66
Joseph Miller <joff@googlehax.com>
2016-11-02 11:31:45 +1100

    Fix indentation more, add colours for AWS

--- segments/aws_profile.py
+++ segments/aws_profile.py
@@ -1,9 +1,9 @@
 import os
 
 def add_aws_profile_segment(powerline):
-  env = os.getenv('AWS_CONFIG_FILE')
-  if env is None:
-    return
+    env = os.getenv('AWS_CONFIG_FILE')
+    if env is None:
+        return
 
-  default_profile = os.getenv('AWS_DEFAULT_PROFILE') or 'default'
-  powerline.append(' %s ' % default_profile )
+    aws_profile = os.getenv('AWS_DEFAULT_PROFILE') or 'default'
+    powerline.append(' %s ' % aws_profile, Color.SSH_FG, Color.SSH_BG )
