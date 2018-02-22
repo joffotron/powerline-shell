166a8aa17ef8273c8b8099004d0d2af30ceff937
Joseph Miller <joff@joff.codes>
2018-02-22 11:34:44 +1100

    Change to using awsp

--- segments/aws_profile.py
+++ segments/aws_profile.py
@@ -1,9 +1,5 @@
 import os
 
 def add_aws_profile_segment(powerline):
-    env = os.getenv('AWS_CONFIG_FILE')
-    if env is None:
-        return
-
-    aws_profile = os.getenv('AWS_DEFAULT_PROFILE') or 'default'
+    aws_profile = os.getenv('AWS_PROFILE') or 'default'
     powerline.append(u'\u2601' + ' %s ' % aws_profile, Color.SSH_FG, Color.SSH_BG )
